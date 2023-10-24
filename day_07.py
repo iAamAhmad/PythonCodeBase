34ee830d05c7d73812c789c0db2b34bc30e8420d
iAamAhmad <aghazi1011@gmail.com>
2023-10-24 21:46:21 +0500

    dictionary

---
+++ day_07.py
@@ -0,0 +1,16 @@
+# Dictionaries in Python!
+## Typing is important!
+from typing import Dict
+
+
+data : list[str] = ["Ahmad","Muhammad", "Faisal"]
+
+data : Dict[str,str] = {
+   "fname" : "Muhammad",
+   "lname" : "Ahmad",
+   "fathersname" : "Muhammad Younas",
+}
+
+print(data["fname"])
+print(data)
+
