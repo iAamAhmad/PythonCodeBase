b06ac19fcb61515abe627a19a7918718d6fe6c77
iAamAhmad <aghazi1011@gmail.com>
2023-11-15 21:10:32 +0500

    python pandas

---
+++ day_19.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pandera as pa
+
+l1 : list[int] = [1,2,3,4,5,6,7,8,9]
+l1
+
+
+s1 : pd.Series = pd.Series([1,2,3,4,5])
+s1
+
+s1 : pd.Series = pd.Series({"a": 10,
+                            "b":20,
+                            "c":30,
+                            "d":40,
+                            "e":50,
+                            "f":60,
+                            "g":70,})
+s1
